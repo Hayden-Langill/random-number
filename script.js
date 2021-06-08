265455b35b4d4a3f1dcef3f2d89a22331ef61456
HaydenLangill <>
2021-06-08 13:51:39 +0000

    stuff

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function guess () {
   random = Math.floor(Math.random() * 6) + 1
   document.getElementById('output').innerHTML = random
   if (number === random) {
-    document.getElementById('output').innerHTML = "Great guess!"
+    document.getElementById('output').innerHTML = 'Great guess!'
     document.getElementById('body').style.backgroundColor = 'green'
-    setTimeout(function(){
+    setTimeout(function () {
       document.getElementById('body').style.backgroundColor = 'white'
     }, 1000)
   }
-  if (number != random) {
+  if (number !== random) {
     document.getElementById('body').style.backgroundColor = 'red'
-    document.getElementById('output').innerHTML = "Wrong"
-    setTimeout(function(){
+    document.getElementById('output').innerHTML = 'Wrong'
+    setTimeout(function () {
       document.getElementById('body').style.backgroundColor = 'white'
     }, 1000)
   }
